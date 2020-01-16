dddb6a53dc6534849674734e821cd98c932ea4ad
Richard Patching <richard@justaddpixels.com>
2020-01-16 15:52:18 +0000

    Move player

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,36 @@ struct Position {
     y: i32,
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+        player_input(self, ctx);
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -36,10 +63,12 @@ struct State {
 }
 
 fn main() {
-    let context = Rltk::init_simple8x8(80, 50, "Dark Metropolis", "resources");
+    let context = Rltk::init_simple8x8(80, 60, "Dark Metropolis", "resources");
     let mut gs = State { ecs: World::new() };
+
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
     gs.ecs
       .create_entity()
       .with(Position { x: 40, y: 25 })
@@ -48,6 +77,7 @@ fn main() {
           fg: RGB::named(rltk::YELLOW),
           bg: RGB::named(rltk::BLACK),
       })
+      .with(Player{})
       .build();
     rltk::main_loop(context, gs);
 }
