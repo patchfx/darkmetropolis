34339919b80e4ba63b37fd5db0d90c21d390f412
Richard Patching <richard@justaddpixels.com>
2020-01-19 15:46:33 +0000

    Extract players and components

---
+++ src/component.rs
@@ -0,0 +1,19 @@
+use specs::prelude::*;
+use rltk::{ RGB };
+
+#[derive(Component)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Component, Debug)]
+pub struct Player {}
+
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: u8,
+    pub fg: RGB,
+    pub bg: RGB,
+}
--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,16 @@ use std::cmp::{max, min};
 extern crate specs_derive;
 
 mod map;
-
 pub use map::*;
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
+mod player;
+pub use player::*;
 
-#[derive(Component, Debug)]
-struct Player {}
+mod component;
+pub use component::*;
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
+pub struct State {
+    ecs: World,
 }
 
 impl GameState for State {
@@ -60,17 +33,6 @@ impl GameState for State {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
     }
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: u8,
-    fg: RGB,
-    bg: RGB,
-}
-
-struct State {
-    ecs: World,
 }
 
 
